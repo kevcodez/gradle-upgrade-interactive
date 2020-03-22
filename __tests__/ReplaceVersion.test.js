126baee1f41d8aa9854d3777b5c95ac778dd5d90
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2020-03-22 13:33:32 +0100

    Fix tests

--- __tests__/ReplaceVersion.test.js
+++ __tests__/ReplaceVersion.test.js
@@ -8,12 +8,12 @@ const dependency = {
 }
 
 test('Replace inline version', () => {
-    const replacedVersion = ReplaceVersion.replace(`compile 'de.kevcodez:pubg-api-wrapper:0.8.1'`, dependency)
+    const replacedVersion = replaceText(`compile 'de.kevcodez:pubg-api-wrapper:0.8.1'`, dependency)
 
     expect(replacedVersion).toBe(`compile 'de.kevcodez:pubg-api-wrapper:1.0.0'`)
 })
 test('Replace version with variable and single quotation marks', () => {
-    const replacedVersion = ReplaceVersion.replace(`
+    const replacedVersion = replaceText(`
         myVar = '0.8.1'
         compile 'de.kevcodez:pubg-api-wrapper:$myVar'
 `, dependency)
@@ -21,7 +21,7 @@ test('Replace version with variable and single quotation marks', () => {
     expect(replacedVersion).toContain(`myVar = '1.0.0'`)
 })
 test('Replace version with variable and double quotation marks', () => {
-    const replacedVersion = ReplaceVersion.replace(`
+    const replacedVersion = replaceText(`
         myVar = "0.8.1"
         compile 'de.kevcodez:pubg-api-wrapper:$myVar'
 `, dependency)
@@ -29,7 +29,7 @@ test('Replace version with variable and double quotation marks', () => {
     expect(replacedVersion).toContain(`myVar = "1.0.0"`)
 })
 test('Replace version with variable with parenthensis and double quotation marks', () => {
-    const replacedVersion = ReplaceVersion.replace(`
+    const replacedVersion = replaceText(`
         myVar = "0.8.1"
         compile 'de.kevcodez:pubg-api-wrapper:\$\{myVar\}'
 `, dependency)
@@ -37,7 +37,7 @@ test('Replace version with variable with parenthensis and double quotation marks
     expect(replacedVersion).toContain(`myVar = "1.0.0"`)
 })
 test('Replace leave variable if it is already updated', () => {
-    const replacedVersion = ReplaceVersion.replace(`
+    const replacedVersion = replaceText(`
         myVar = "1.0.0"
         compile 'de.kevcodez:pubg-api-wrapper:\$\{myVar\}'
 `, dependency)
@@ -51,7 +51,7 @@ test('Replace plugin version with single quotation marks', () => {
         oldVersion: '0.21.0',
         version: '0.22.0'
     }
-    const replacedVersion = ReplaceVersion.replace(`id 'com.github.ben-manes.versions' version '0.21.0'`, pluginDependency)
+    const replacedVersion = replaceText(`id 'com.github.ben-manes.versions' version '0.21.0'`, pluginDependency)
 
     expect(replacedVersion).toBe(`id 'com.github.ben-manes.versions' version '0.22.0'`)
 })
@@ -62,12 +62,12 @@ test('Replace plugin version with double quotation marks', () => {
         oldVersion: '0.21.0',
         version: '0.22.0'
     }
-    const replacedVersion = ReplaceVersion.replace(`id 'com.github.ben-manes.versions' version "0.21.0"`, pluginDependency)
+    const replacedVersion = replaceText(`id 'com.github.ben-manes.versions' version "0.21.0"`, pluginDependency)
 
     expect(replacedVersion).toBe(`id 'com.github.ben-manes.versions' version "0.22.0"`)
 })
 test('Replace version with version prefix in dependency', () => {
-    const replacedVersion = ReplaceVersion.replace(`compile group: 'de.kevcodez.pubg', name: 'pubg-api-wrapper', version: '0.8.1'`, dependency)
+    const replacedVersion = replaceText(`compile group: 'de.kevcodez.pubg', name: 'pubg-api-wrapper', version: '0.8.1'`, dependency)
 
     expect(replacedVersion).toBe(`compile group: 'de.kevcodez.pubg', name: 'pubg-api-wrapper', version: '1.0.0'`)
 })
@@ -78,6 +78,17 @@ test('Replace kotlin plugin version', () => {
         oldVersion: '0.22.0',
         version: '0.24.0'
     }
-    const replacedVersion = ReplaceVersion.replace(`id("com.github.ben-manes.versions") version "0.22.0"`, pluginDependency)
+    const replacedVersion = replaceText(`id("com.github.ben-manes.versions") version "0.22.0"`, pluginDependency)
     expect(replacedVersion).toBe(`id("com.github.ben-manes.versions") version "0.24.0"`)
 })
+
+function replaceText(source, dependency) {
+    const replaceVersionActions = ReplaceVersion.replace(source, dependency)
+
+    let modifiedSource = source
+    replaceVersionActions.forEach(replaceAction => {
+        modifiedSource = modifiedSource.replace(replaceAction.searchValue, replaceAction.replaceValue)
+    })
+
+    return modifiedSource
+}
